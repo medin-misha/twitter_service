6d69e6917f7efe1023d4f47ebd323125c2f63c04
misha <medinskijmisa228@gmail.com>
2025-03-14 16:07:06 +0200

    add cors settings and image_service envs

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from fastapi.staticfiles import StaticFiles
 import uvicorn
 from pathlib import Path
 from handlers import user_router, media_router, tweet_router, get_image
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
@@ -11,6 +13,13 @@ app.include_router(user_router)
 app.include_router(media_router)
 app.include_router(tweet_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешить все источники 
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешить все методы (GET, POST и т. д.)
+    allow_headers=["*"],  # Разрешить все заголовки
+)
 
 @app.get("/me", response_class=HTMLResponse)
 async def main():
--- app/run.sh
+++ app/run.sh
@@ -3,6 +3,8 @@
 export postgres_user=postgres_user
 export postgres_password=postgres_password
 export postgres_host=192.168.5.197:5432/postgres_db
+
+export image_service_host=http://192.168.5.197:9000/
 # Выполнение миграций
 alembic upgrade head
 
